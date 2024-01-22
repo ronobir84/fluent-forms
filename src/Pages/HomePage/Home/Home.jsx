e9cbce8455eef85d9b2f98260e81257312f2c084
ronodir84 <priashdas@84gmail.com>
2024-01-22 15:16:56 +0600

    setup te fluent-forms websites

---
+++ src/Pages/HomePage/Home/Home.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Hello</h3>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ 
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LayOut from './LayOut/LayOut.jsx';
+import Home from './Pages/HomePage/Home/Home.jsx';
+ const router = createBrowserRouter([
+  {
+    path: "/",
+     element: <LayOut></LayOut>,
+     children: [
+       {
+         path: "/",
+         element : <Home></Home>
+      }
+    ]
+   },
+   
+   
+]);
+<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='container max-w-full'>
+      <RouterProvider router={router} />
+   </div>
+  </React.StrictMode>,
+)
