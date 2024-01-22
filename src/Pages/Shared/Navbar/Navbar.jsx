bbb45940ec14945c33b39d3d43bfc6def22a4db9
ronodir84 <priashdas@84gmail.com>
2024-01-22 22:53:29 +0600

    add the nevbar section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {  NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import logo from "../../../assets/image/logo.png"
+import { FaAngleDown } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <div>
             <div>
-                <div className='flex justify-between bg-[#012A5B] h-[70px] w-full items-center px-10 fixed'>
+                <div className='flex justify-between bg-[#012A5B] h-[70px] w-full items-center px-10 fixed '>
                     <div className='flex items-center gap-5'>
                         <button className='text-xl font-medium bg-[#084A75] text-[#06CDFF] px-7 py-2 rounded-3xl'>New Release</button>
                         <h2 className='text-lg text-white font-normal'>Fluent Forms 3.4 ......</h2>
@@ -17,7 +19,36 @@ const Navbar = () => {
                             <li> <NavLink className="hover:text-[#06CDFF] hover:duration-700">Supports</NavLink></li>
                         </ul>
                     </div>
+                </div>
+                <div className='pt-[70px]'>
+                    <div className='bg-[#012A5B] h-[90px] w-full fixed border-solid border-2 border-[#115382] flex justify-between items-center px-10'>
+                        <div className='flex items-center' >
+                            <img className='w-12' src={logo} alt="" />
+                            <h2><span className='text-2xl text-white font-semibold '>Fluent</span><span className='text-2xl text-gray-400'>Forms</span></h2>
 
+
+                             
+                            <div className='flex  items-center relative left-[35%]'>
+                                <div className="dropdown ">
+                                    <div tabIndex={0} role="button" className="pl-6 m-1 flex items-center gap-2 "><span className='text-xl font-medium text-white    '>Features</span><FaAngleDown className='text-xl font-medium text-white  '></FaAngleDown></div>
+                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-white  rounded-box w-36 ">
+                                        <li className='text-[#06CDFF] text-xl font-medium'><a>Item 1</a></li>
+                                        <li className='text-[#06CDFF] text-xl font-medium'><a>Item 2</a></li>
+                                    </ul>
+                                </div>
+                                <ul className='flex gap-12 pl-7'>
+                                    <li><NavLink className="text-xl font-medium text-white ">Integrations</NavLink></li>
+                                    <li><NavLink className="text-xl font-medium text-white ">Blog</NavLink></li>
+                                    <li><NavLink className="text-xl font-medium text-white "> Pricing</NavLink></li>
+                                </ul>
+                            </div>
+                               </div>
+
+                        <div>
+                            <button className='min-bg px-8 font-bold text-white py-3 rounded text-xl border-solid border-2 border-[#5680B1]'>Get Fluent Forms</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
