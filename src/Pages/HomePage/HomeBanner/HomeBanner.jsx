670b10f2646d7bab507f3ca47e44a1e5222b112b
ronodir84 <priashdas@84gmail.com>
2024-01-23 19:14:20 +0600

    add the second navbar section

--- src/Pages/HomePage/HomeBanner/HomeBanner.jsx
+++ src/Pages/HomePage/HomeBanner/HomeBanner.jsx
@@ -1,7 +1,11 @@
 import { FaStar } from "react-icons/fa6";
 import { MdVerifiedUser } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
-import { IoDocumentTextSharp } from "react-icons/io5";
+import { IoDocumentTextSharp, IoEyeSharp, IoScan } from "react-icons/io5";
+import { CiMenuKebab } from "react-icons/ci";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 const HomeBanner = () => {
     return (
         <div className="bg-[#00295A]   w-full min-h-screen">
@@ -18,7 +22,7 @@ const HomeBanner = () => {
                         <button className="bg-[#0976FF] px-6 font-semibold py-4 text-xl text-white rounded-md">Get Fluent Forms</button>
                         <button className="bg-[#fff] text-xl font-semibold px-10 py-4 text-[#00295A] rounded-md border-solid border-2 border-[#5680B1]">View Demos</button>
                     </div>
-                    <div className="flex gap-8 justify-center">
+                    <div className="flex gap-8 justify-center pb-8 pt-8">
                         <div className="flex gap-4 items-center">
                             <div>
                                 <FaStar className="text-6xl p-3 rounded-full border-4 border-white text-white"></FaStar>
@@ -69,9 +73,48 @@ const HomeBanner = () => {
                         </div>
                     </div>
 
-                    <div className="w-[78%] h-[600px] bg-white mx-auto">
+                    <div className="w-[86%] h-[600px] bg-white mx-auto  relative z-20 rounded">
                         <div>
+                            <div className="w-full h-24 border-b-2 border-gray-300 flex items-center justify-between px-6">
+                                <div className="flex gap-3">
+                                    <FaPencil className="text-3xl  text-[#626261]"></FaPencil>
+                                    <p className="text-xl  text-black">Contact Form...</p>
+                                </div>
+                                <div className="pr-16 ">
+                                    <ul className="flex gap-4">
+                                        <li><Link className="text-xl text-black font-medium">Editor</Link></li>
+                                        <li><Link className="text-xl text-black font-medium">Design</Link></li>
+                                        <li><Link className="text-xl text-black font-medium">Settings & Integration</Link> </li>
+                                        <li> <Link className="text-xl text-black font-medium">Entries</Link></li>
+                                    </ul>
+                                </div>
+                                <div className="flex gap-3">
+                                    <div>
+                                        <button className="flex gap-3 bg-[#F2F2F2] px-6 text-[#5E5D5C] rounded py-3">
+                                             <IoDocumentTextSharp className="text-2xl"></IoDocumentTextSharp>
+                                            <span className="text-xl">[fluentform id=”9”]</span>
+                                        </button>
+                                    </div>
 
+                                    <div>
+                                        <button className="flex gap-3 bg-[#F2F2F2] px-6 text-[#5E5D5C] rounded py-3">
+                                             <IoEyeSharp className="text-2xl"></IoEyeSharp>
+                                            <span className="text-xl">Preview</span>
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button className="flex gap-3 bg-[#8EC0FF] px-6 text-white rounded py-3">
+                                             <FaCheckCircle className="text-2xl"></FaCheckCircle>
+                                            <span className="text-xl">Save Form</span>
+                                        </button>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <IoScan className="text-4xl text-[#626261] "></IoScan>
+                                        <CiMenuKebab className="text-4xl text-[#626261]"></CiMenuKebab>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
