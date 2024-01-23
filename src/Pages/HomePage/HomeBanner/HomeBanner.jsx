47467433fcffb464d7089bccaab5c0ad7092c8d7
ronodir84 <priashdas@84gmail.com>
2024-01-23 12:07:29 +0600

    add the icon banner section

--- src/Pages/HomePage/HomeBanner/HomeBanner.jsx
+++ src/Pages/HomePage/HomeBanner/HomeBanner.jsx
@@ -1,4 +1,7 @@
 import { FaStar } from "react-icons/fa6";
+import { MdVerifiedUser } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
+import { IoDocumentTextSharp } from "react-icons/io5";
 const HomeBanner = () => {
     return (
         <div className="bg-[#00295A] w-full min-h-screen">
@@ -14,10 +17,44 @@ const HomeBanner = () => {
                     </div>
                     <div>
                         <div>
-
+                            <FaStar className="text-7xl p-3 rounded-full border-4 border-white text-white"></FaStar>
                         </div>
-                        <div></div>
+                        <div>
+                            <h2 className="text-3xl font-bold text-white">350+ </h2>
+                            <p className="text-xl font-medium text-white">Five Star Reviews</p>
+                        </div>
                     </div>
+
+                    <div>
+                        <div>
+                            <MdVerifiedUser className="text-7xl p-3 rounded-full border-4 border-white text-white"></MdVerifiedUser>
+                        </div>
+                        <div>
+                            <h2 className="text-3xl font-bold text-white">14 Days  </h2>
+                            <p className="text-xl font-medium text-white">Money Back Guarantee</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <IoMdSettings className="text-7xl p-3 rounded-full border-4 border-white text-white"></IoMdSettings>
+                        </div>
+                        <div>
+                            <h2 className="text-3xl font-bold text-white">300K+ </h2>
+                            <p className="text-xl font-medium text-white">Active Installation</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <IoDocumentTextSharp className="text-7xl p-3 rounded-full border-4 border-white text-white"></IoDocumentTextSharp>
+                        </div>
+                        <div>
+                            <h2 className="text-3xl font-bold text-white">200+  </h2>
+                            <p className="text-xl font-medium text-white">Forms Demos</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
