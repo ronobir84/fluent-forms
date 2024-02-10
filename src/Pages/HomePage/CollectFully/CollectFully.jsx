bfd1a3dc4e36e6c6d5f071e8cac3e2d1222d4895
ronodir84 <priashdas@84gmail.com>
2024-02-10 11:25:04 +0600

    add this mainsection

--- src/Pages/HomePage/CollectFully/CollectFully.jsx
+++ src/Pages/HomePage/CollectFully/CollectFully.jsx
@@ -7,32 +7,32 @@ import Icon5 from "../../../assets/image/wire--icon.png"
 import Icon6 from "../../../assets/image/dolore-icon.png"
  const CollectFully = () => {
     return (
-        <div className="py-8">
-            <div>
+        <div className="py-8 px-[100px]">
+            <div className="flex gap-12">
                 {/* first section */}
                 <div>
-                    <div className="bg-[#00295A] w-[650px] h-[500px]">
+                    <div className="bg-[#00295A] w-[700px] h-[500px] rounded-md">
                         <div className="px-10 space-y-2 pt-10">
                             <h2 className="text-2xl font-semibold text-white">Collect payments and donations</h2>
                             <p className="text-lg font-medium text-gray-300">Put anything in a dropdown - forms, images, videos, or any other Breakdance elements.</p>
                         </div>
                         
                         <div className="">
-                            <div className="w-56 main-op h-56 absolute border-solid border-[17px] border-[#06CDFF] rounded-full border-opacity-15 ml-[24%]">
+                            <div className="w-60 main-op h-60 absolute border-solid border-[17px] border-[#06CDFF] rounded-full border-opacity-15 ml-[22%]">
 
                             </div>
                             <div>
-                                <img className=" relative  top-[98px]" src={worldImage} alt="" />
+                                <img className=" relative  top-[98px] left-9" src={worldImage} alt="" />
                             </div>
                             <div>
                                 <div className="flex">
                                     <img className="relative w-20 bottom-36 left-[55%]  " src={Icon1} alt="" />
                                     <img className="relative w-20 bottom-36 left-[23%] " src={Icon2} alt="" />
-                                    <img className="w-20 relative right-24 bottom-10" src={Icon4} alt="" />
+                                    <img className="w-20 relative right-16 bottom-10" src={Icon4} alt="" />
                                     
-                                    <img className="w-20 relative right-10" src={Icon3} alt="" />
-                                    <img className="relative bg-main left-5 bottom-10    w-[70px] h-[70px] p-4 rounded-md" src={Icon5} alt="" />
-                                    <img className="relative bg-main left-24 bottom-2    w-[70px] h-[70px] p-4 rounded-md" src={Icon6} alt="" />
+                                    <img className="w-20 relative right-4" src={Icon3} alt="" />
+                                    <img className="relative bg-main left-10 bottom-8    w-[70px] h-[70px] p-4 rounded-md" src={Icon5} alt="" />
+                                    <img className="relative bg-main left-32 bottom-2    w-[70px] h-[70px] p-4 rounded-md" src={Icon6} alt="" />
                                 </div>
                                  
                                 
@@ -48,7 +48,11 @@ import Icon6 from "../../../assets/image/dolore-icon.png"
 
 
                 {/* second section */}
-                <div></div>
+                <div>
+                    <div className="w-[750px] h-[500px] bg-[#F4FBFF]">
+                         
+                    </div>
+                </div>
             </div>
         </div>
     );
