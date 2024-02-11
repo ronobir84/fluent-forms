820f47712570a4867f1c58d6d8c12c4f8a9a478d
ronodir84 <priashdas@84gmail.com>
2024-02-11 22:19:17 +0600

    add this icon

--- src/Pages/HomePage/Fully/Fully.jsx
+++ src/Pages/HomePage/Fully/Fully.jsx
@@ -1,10 +1,12 @@
- 
+ import icon1 from "../../../assets/image/res-icon1.png"
+ import icon2 from "../../../assets/image/res-icon2.png"
+ import icon3 from "../../../assets/image/res-icon4.png"
 const Fully = () => {
     return (
         <div>
             <div>
                 {/* first card */}
-                <div className="w-80 h-80 bg-[#F4FBFF]">
+                <div className="w-80 h-64 bg-[#F4FBFF] rounded">
                     <div>
 
                     </div>
