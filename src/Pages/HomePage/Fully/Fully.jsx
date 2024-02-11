1f7844d6ed7b2399a7890af63bd36994987169ef
ronodir84 <priashdas@84gmail.com>
2024-02-11 22:25:20 +0600

    add this first card

--- src/Pages/HomePage/Fully/Fully.jsx
+++ src/Pages/HomePage/Fully/Fully.jsx
@@ -1,13 +1,21 @@
  import icon1 from "../../../assets/image/res-icon1.png"
  import icon2 from "../../../assets/image/res-icon2.png"
- import icon3 from "../../../assets/image/res-icon4.png"
+ import icon3 from "../../../assets/image/res-icon3.png"
 const Fully = () => {
     return (
         <div>
             <div>
                 {/* first card */}
-                <div className="w-80 h-64 bg-[#F4FBFF] rounded">
-                    <div>
+                <div className="w-80 h-64 bg-[#F4FBFF] rounded-md">
+                    <div className="px-5">
+                        <div className="pt-12">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="pt-8 space-y-3">
+                            <h2 className="text-2xl font-semibold text-black">Fully mobile responsive</h2>
+                            <p className="text-base text-black  font-medium">Put anything in a dropdown - forms, images, videos, or any other.</p>
+                       </div>
+                        
 
                     </div>
 
