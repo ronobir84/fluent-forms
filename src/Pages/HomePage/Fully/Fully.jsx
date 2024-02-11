2ff8841eb4abb12406178a84cc6e815417127b2e
ronodir84 <priashdas@84gmail.com>
2024-02-11 22:26:57 +0600

    add this second card

--- src/Pages/HomePage/Fully/Fully.jsx
+++ src/Pages/HomePage/Fully/Fully.jsx
@@ -20,6 +20,22 @@ const Fully = () => {
                     </div>
 
                 </div>
+
+                {/* second card */}
+                <div className="w-80 h-64 bg-[#F4FBFF] rounded-md">
+                    <div className="px-5">
+                        <div className="pt-12">
+                            <img src={icon2} alt="" />
+                        </div>
+                        <div className="pt-8 space-y-3">
+                            <h2 className="text-2xl font-semibold text-black">Send notifications. Instantly</h2>
+                            <p className="text-base text-black  font-medium">Put anything in a dropdown - forms, images, videos, or any other.</p>
+                        </div>
+
+
+                    </div>
+
+                </div>
                  
             </div>
         </div>
