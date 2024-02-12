46b0649e12c27f503ec4bc24a994fa64ba287643
ronodir84 <priashdas@84gmail.com>
2024-02-12 13:15:19 +0600

    add this card section

--- src/Pages/HomePage/Connect/Connect.jsx
+++ src/Pages/HomePage/Connect/Connect.jsx
@@ -4,7 +4,19 @@
     return (
         <div>
             <div>
-                <div className="w-[500px] h-[350px] bg-[#00295A]">
+                <div className="w-[600px] h-[260px] bg-[#00295A]">
+                    <div className="flex justify-between">
+                        <div className="pt-16 pl-4 space-y-3">
+                            <h2 className="text-2xl text-white font-semibold">Connect to CRM</h2>
+                            <p className="text-white font-normal text-base">Your favorite CRM tool is one step away <br /> from connecting with Fluent Forms. <br /> Don’t lose another lead.</p>
+                        </div>
+                        <div className="w-[270px] h-[250px] bg-[#163B67] rounded-lg ">
+
+                        </div>
+                        <div className="w-64 h-[230px] bg-[#F4FBFF] absolute left-[20.6%]  rounded-md shadow-lg">
+
+                        </div>
+                    </div>
 
                 </div>
             </div>
