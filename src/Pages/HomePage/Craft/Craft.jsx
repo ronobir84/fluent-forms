e7590c25c3e990ec8bcff66f1989736863576379
ronodir84 <priashdas@84gmail.com>
2024-02-14 10:25:26 +0600

    finsed the craft section

--- src/Pages/HomePage/Craft/Craft.jsx
+++ src/Pages/HomePage/Craft/Craft.jsx
@@ -3,18 +3,35 @@ import { FaCheck } from "react-icons/fa";
 const Craft = () => {
 
     return (
-        <div className="mx-[105px] pr-5 relative right-2">
-            <div className="w-full h-28 bg-[#0976FF] ">
-                <div>
-                    <div>
+        <div className="mx-[105px] pr-5 relative right-2 py-6">
+            <div className="w-full h-36 bg-[#0976FF] rounded">
+                <div className="flex justify-between items-center px-10 pt-8">
+                    <div className="space-y-3">
                         <div>
                             <h2 className="text-4xl font-bold text-white">Craft amazing web forms!</h2>
                         </div>
-                        <div>
+                        <div className="flex gap-6">
+                            <div className="flex items-center gap-2">
+                                <FaCheck className="text-white text-base"></FaCheck>
+                                <p className="text-base text-white font-medium">Use on unlimited sites</p>
 
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <FaCheck className="text-white text-base"></FaCheck>
+                                <p className="text-base text-white font-medium">Use on unlimited sites</p>
+
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <FaCheck className="text-white text-base"></FaCheck>
+                                <p className="text-base text-white font-medium">Use on unlimited sites</p>
+
+                            </div>
                         </div>
                         
-                     </div>
+                    </div>
+                    <div>
+                        <button className="px-7 py-4 bg-[#00295A] text-lg font-semibold text-white rounded">See All Templates</button>
+                    </div>
                 </div>
 
             </div>
