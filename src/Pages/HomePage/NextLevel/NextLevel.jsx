e9586ffb7142ccfea508b72d08592a951723b1cd
ronodir84 <priashdas@84gmail.com>
2024-02-15 13:49:56 +0600

    add this hading

--- src/Pages/HomePage/NextLevel/NextLevel.jsx
+++ src/Pages/HomePage/NextLevel/NextLevel.jsx
@@ -2,7 +2,20 @@
 const NextLevel = () => {
     return (
         <div>
-            <div className="min-h-screen">
+            <div className="min-h-screen bg-[#00295A]">
+                <div className="w-72 h-72 bl-min bg-[#2FF3FF] absolute   right-0 mt-[17%]  rounded-full ">
+
+                </div>
+                <div className="w-72 h-72 bl-min bg-[#2FF3FF] absolute   mt-[25%]  rounded-full ">
+
+                </div>
+                <div>
+                    <div>
+                        <h1 className="text-5xl font-bold text-white">The <span className="text-[#0976FF]">Next Level</span> Experience</h1>
+                        <p className="text-white text-base font-normal">An <span className="text-[#0976FF]">independent research</span> shows how form plugins load assets. Hint: The lower the better.</p>
+                    </div>
+                </div>
+
 
             </div>
         </div>
