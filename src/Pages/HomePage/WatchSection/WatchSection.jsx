bf4fea4c3415ed848286c8cf0664544d8b1f4f07
ronodir84 <priashdas@84gmail.com>
2024-02-26 00:04:40 +0600

    add this first Watch section

--- src/Pages/HomePage/WatchSection/WatchSection.jsx
+++ src/Pages/HomePage/WatchSection/WatchSection.jsx
@@ -1,7 +1,18 @@
  
 const WatchSection = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-[#F4FBFF]">
+            <div>
+                <div className="flex justify-between px-[100px] pt-16">
+                    <div>
+                        <h2 className="text-3xl font-bold text-[#000] ">Watch Fluent Forms in action!</h2>
+                        <p className="text-xl font-medium text-[#000]">An independent research shows how form plugins load assets.</p>
+                    </div>
+                    <div>
+                        <button className="px-6 py-3 bg-[#0976FF] rounded text-lg font-semibold text-white">See All Templates</button>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
