ae6a3dfce72b12cc57a71f37a68beb14f0c23fe9
ronodir84 <priashdas@84gmail.com>
2024-02-26 23:20:58 +0600

    add this first button

--- src/Pages/HomePage/WatchSection/WatchSection.jsx
+++ src/Pages/HomePage/WatchSection/WatchSection.jsx
@@ -1,4 +1,4 @@
- 
+import { FiFileText } from "react-icons/fi";
 const WatchSection = () => {
     return (
         <div className="min-h-screen bg-[#F4FBFF]">
@@ -11,6 +11,41 @@ const WatchSection = () => {
                     <div>
                         <button className="px-6 py-3 bg-[#0976FF] rounded text-lg font-semibold text-white">See All Templates</button>
                     </div>
+                </div>
+
+
+
+                <div>
+                    <div>
+                        {/* first section */}
+                        <div>
+                            <div>
+                                <div className="">
+                                    <button className="flex w-96  h-16 bg-[#00295A] justify-between items-center  p-4 rounded-md">
+                                        <div className="flex items-center gap-6">
+                                            <FiFileText className="text-3xl text-[#FFFFFF] "></FiFileText>
+                                            <span className="text-xl text-[#FFFFFF] font-medium">Contact Form</span>
+                                        </div>
+                                        <div>
+                                            <button className="w-11 h-11 text-xl bg-[#FFFFFF] rounded-full p-1 text-black font-semibold">02</button>
+                                        </div>
+                                    </button>
+                                    
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+
+
+                        {/* second section */}
+                        <div>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
             
