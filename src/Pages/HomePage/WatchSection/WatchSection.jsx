785f26e5105beec3b816e1bf884cea6c017434cd
ronodir84 <priashdas@84gmail.com>
2024-02-26 23:35:33 +0600

    add this six button

--- src/Pages/HomePage/WatchSection/WatchSection.jsx
+++ src/Pages/HomePage/WatchSection/WatchSection.jsx
@@ -77,12 +77,26 @@ const WatchSection = () => {
                                     
                                 </div>
 
-                                {/* four button */}
+                                {/*  five button */}
                                 <div className="mt-2">
                                     <button className="flex w-96  h-16 bg-[#FFF] justify-between items-center  p-4 rounded-md">
                                         <div className="flex items-center gap-6">
                                             <FiFileText className="text-3xl text-[#001222] "></FiFileText>
                                             <span className="text-xl text-[#001222] font-medium">Conditional Form</span>
+                                        </div>
+                                        <div>
+                                            <button className="w-11 h-11 text-xl bg-[#E6E6E6] rounded-full p-1 text-[#868686] font-semibold">02</button>
+                                        </div>
+                                    </button>
+                                    
+                                </div>
+
+                                {/*  six button */}
+                                <div className="mt-2">
+                                    <button className="flex w-96  h-16 bg-[#FFF] justify-between items-center  p-4 rounded-md">
+                                        <div className="flex items-center gap-6">
+                                            <FiFileText className="text-3xl text-[#001222] "></FiFileText>
+                                            <span className="text-xl text-[#001222] font-medium">Volunteer Application</span>
                                         </div>
                                         <div>
                                             <button className="w-11 h-11 text-xl bg-[#E6E6E6] rounded-full p-1 text-[#868686] font-semibold">02</button>
