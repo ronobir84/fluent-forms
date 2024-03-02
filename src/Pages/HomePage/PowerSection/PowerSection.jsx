1279a692475a61ae244c5e20955041257fc22955
ronodir84 <priashdas@84gmail.com>
2024-03-02 13:53:03 +0600

    add this power first item

--- src/Pages/HomePage/PowerSection/PowerSection.jsx
+++ src/Pages/HomePage/PowerSection/PowerSection.jsx
@@ -1,10 +1,11 @@
- 
+ import groupImg from "../../../assets/image/Group.png"
 const PowerSection = () => {
     return (
         <div>
             <div className="w-full h-[520px] bg-[#F4FBFF]">
                 <div>
-                    
+                    <img className="w-32 relative mx-auto bottom-16 right-20" src={groupImg} alt="" />
+
                 </div>
 
             </div>
