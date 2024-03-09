31dd0c9c940470ebb75a577834ef58c3bf83a97a
ronodir84 <priashdas@84gmail.com>
2024-03-09 23:58:31 +0600

    add this main section

--- src/Pages/HomePage/Frequently/Frequently.jsx
+++ src/Pages/HomePage/Frequently/Frequently.jsx
@@ -1,10 +1,24 @@
- 
+import { FaAngleDown } from "react-icons/fa6";
+
 const Frequently = () => {
     return (
         <div className="w-full min-h-screen bg-[#F4FBFF]">
             <div className="text-center pt-16 space-y-2">
                 <h2 className="text-3xl text-black font-semibold">Frequently Asked Questions</h2>
                 <p className="text-lg text-black">An independent research shows how form plugins load assets.</p>
+            </div>
+            <div className="text-center pt-8 relative left-[20%]">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="pl-6 m-1 flex items-center gap-2 px-8  py-4 border-solid border-2 border-[#EAEAEA] bg-[#FFFFFF]  rounded "><span className='text-xl font-medium text-black    '>Install & Setup</span><FaAngleDown className='text-xl font-medium text-black  '></FaAngleDown></div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-white  rounded-box w-36 ">
+                        <li className='text-[#06CDFF] text-xl font-medium'><a>Item 1</a></li>
+                        <li className='text-[#06CDFF] text-xl font-medium'><a>Item 2</a></li>
+                    </ul>
+                </div>
+            </div> 
+
+            <div>
+                
             </div>
         </div>
     );
