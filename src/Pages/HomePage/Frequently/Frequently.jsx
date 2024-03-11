484fab31e848d279ee4cdc8e94303e2e2b6bea66
ronodir84 <priashdas@84gmail.com>
2024-03-11 22:56:37 +0600

    add this last section

--- src/Pages/HomePage/Frequently/Frequently.jsx
+++ src/Pages/HomePage/Frequently/Frequently.jsx
@@ -7,19 +7,17 @@ const Frequently = () => {
                 <h2 className="text-3xl text-black font-semibold">Frequently Asked Questions</h2>
                 <p className="text-lg text-black">An independent research shows how form plugins load assets.</p>
             </div>
-            <div className="text-center pt-8 relative left-[20%]">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="pl-6 m-1 flex items-center gap-2 px-8  py-4 border-solid border-2 border-[#EAEAEA] bg-[#FFFFFF]  rounded "><span className='text-xl font-medium text-black    '>Install & Setup</span><FaAngleDown className='text-xl font-medium text-black  '></FaAngleDown></div>
+            <div>
+                <div className="dropdown ">
+                    <div tabIndex={0} role="button" className="pl-6 m-1 flex items-center gap-4 border-solid border-2 border-[#EAEAEA] rounded shadow bg-[#FFFFFF]  px-8 py-3 "><span className='text-xl font-medium text-black   '>Features</span><FaAngleDown className='text-xl font-medium text-black  '></FaAngleDown></div>
                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-white  rounded-box w-36 ">
                         <li className='text-[#06CDFF] text-xl font-medium'><a>Item 1</a></li>
                         <li className='text-[#06CDFF] text-xl font-medium'><a>Item 2</a></li>
                     </ul>
                 </div>
-            </div> 
-
-            <div>
-                
             </div>
+
+             
         </div>
     );
 };
