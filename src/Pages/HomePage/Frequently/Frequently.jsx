31f0b41b7ae6023a6e1a832626e25bfe6c59debb
ronodir84 <priashdas@84gmail.com>
2024-03-12 00:17:03 +0600

    add this min section

--- src/Pages/HomePage/Frequently/Frequently.jsx
+++ src/Pages/HomePage/Frequently/Frequently.jsx
@@ -8,12 +8,50 @@ const Frequently = () => {
                 <p className="text-lg text-black">An independent research shows how form plugins load assets.</p>
             </div>
             <div>
-                <div className="dropdown ">
-                    <div tabIndex={0} role="button" className="pl-6 m-1 flex items-center gap-4 border-solid border-2 border-[#EAEAEA] rounded shadow bg-[#FFFFFF]  px-8 py-3 "><span className='text-xl font-medium text-black   '>Features</span><FaAngleDown className='text-xl font-medium text-black  '></FaAngleDown></div>
+                <div className="dropdown relative left-[74%]">
+                    <div tabIndex={0} role="button" className="pl-10 m-1 flex items-center gap-8 border-solid border-2 border-[#EAEAEA] rounded shadow bg-[#FFFFFF] w-56 h-14  "><span className='text-2xl font-medium text-[#001222]   '>Features</span><FaAngleDown className='text-2xl font-medium text-black  '></FaAngleDown></div>
                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-white  rounded-box w-36 ">
                         <li className='text-[#06CDFF] text-xl font-medium'><a>Item 1</a></li>
                         <li className='text-[#06CDFF] text-xl font-medium'><a>Item 2</a></li>
                     </ul>
+                </div>                
+            </div>
+            <div className="px-[200px] space-y-4 mt-10">
+                <div className="collapse collapse-plus bg-[#FFFFFF] p-3 border-solid border-2 border-[#EAEAEA] shadow">
+                    <input type="radio" name="my-accordion-3" checked="checked" />
+                    <div className="collapse-title text-2xl text-black font-medium">
+                        Who should use Fluent Forms?
+                    </div>
+                    <div className="collapse-content">
+                        <p className="text-base text-black">Fluent Forms is a lightweight and beginner-friendly tool to empower online businesses. You can consider it beyond a form builder, as it helps you grow your business with many cutting-edge features. Whether you want to sell products, collect information, integrate marketing tools, or receive payments — Fluent Forms has a lot to offer.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#FFFFFF] border-solid border-2 border-[#EAEAEA] shadow p-3">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-2xl text-black font-medium">
+                        Do I need coding skills to use Fluent Forms?
+                    </div>
+                    <div className="collapse-content">
+                        <p className="text-base text-black"> it helps you grow your business with many cutting-edge features. Whether you want to sell products, collect information, integrate marketing tools, or receive payments — Fluent Forms has a lot to offer.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#FFFFFF] border-solid border-2 border-[#EAEAEA] shadow p-3">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-2xl text-black font-medium">
+                       Will Fluent Forms affect the speed of my website??
+                    </div>
+                    <div className="collapse-content">
+                        <p className="text-base text-black"> .. collect information, integrate marketing tools, or receive payments — Fluent Forms has a lot to offer.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#FFFFFF] border-solid border-2 border-[#EAEAEA] shadow p-3">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-2xl text-black font-medium">
+                        How can I use Fluent Forms on client sites?
+                    </div>
+                    <div className="collapse-content">
+                        <p className="text-base text-black"> it helps you grow your business with many cutting-edge features. Whether you want to sell products,</p>
+                    </div>
                 </div>
             </div>
 
