c76eb3cff5182645d0eb625dae9c6ea8841034b6
ronodir84 <priashdas@84gmail.com>
2024-03-14 21:15:41 +0600

    add this footer section

--- src/Pages/HomePage/PricingSection/PricingSection.jsx
+++ src/Pages/HomePage/PricingSection/PricingSection.jsx
@@ -1,11 +1,11 @@
  
 const PricingSection = () => {
     return (
-        <div className="w-full min-h-screen bg-[#00295A]">
+        <div className="w-full min-h-screen bg-[#00295A]  pb-28">
             <div>
                 <div className="w-[220px] h-[220px] bl-min bg-[#2FF3FF] absolute  right-0 mt-20      rounded-full "></div>
 
-                <div className="flex justify-between px-[170px] pt-20 ">
+                <div className="flex justify-between px-[200px] pt-20 ">
                     <div className="space-y-5">
                         <h2 className="text-3xl font-semibold text-white">Pricing Section</h2>
                         <p className="text-xl  text-white">Don’t take our words for granted — build any form <br /> instantly using all the features at your disposal. </p>
@@ -16,7 +16,50 @@ const PricingSection = () => {
                         <button className="bg-[#00295A] px-6 py-2 border-solid border-t-4 border-b-4 border-r-4 border-[#0976FF] text-lg text-white rounded-r-md">Lifetime</button>
                     </div>
                 </div>
-                
+
+                <div className="w-[220px] h-[220px] bl-min bg-[#2FF3FF] absolute   mt-60      rounded-full "></div>
+
+                <div className="flex justify-between px-[200px] pt-10">
+
+                    {/* first card */}
+                    <div className="w-72 h-96 bg-[#FFFFFF] rounded-md shadow">
+                        <div className="text-center space-y-4 pt-12">
+                            <h3 className="text-2xl font-semibold text-[#001222]">Single Site License</h3>
+                            <h1 className="text-6xl text-[#001222] font-extrabold">$59</h1>
+                            <h4 className="text-xl text-[#001222] font-medium">Single Site license</h4>
+                            <p className="text-base text-black">1 Year Plugin Update 1 Year Priority Support All Features Included</p>
+                            <button className="text-lg px-5 py-3 bg-[#0976FF] text-white font-medium rounded shadow pt-3">See All Templates</button>
+                        </div>
+                    </div>
+
+                    {/* second card */}
+                    <div className="w-72 h-96 bg-[#0976FF] rounded-md shadow">
+                        <div className="text-center space-y-4 pt-12">
+                            <h3 className="text-2xl font-semibold text-[#FFFFFF]">Agency License</h3>
+                            <h1 className="text-6xl text-[#FFFFFF] font-extrabold">$129</h1>
+                            <h4 className="text-xl text-[#FFFFFF] font-medium">5 Domains License</h4>
+                            <p className="text-base text-white">  1 Year Plugin Update 1 Year Priority Support All Features Included</p>
+                            <button className="text-lg px-5 py-3 bg-[#FFFFFF] text-[#0976FF] font-medium rounded shadow pt-3">See All Templates</button>
+                        </div>
+                    </div>
+
+                    {/* third card */}
+                    <div className="w-72 h-96 bg-[#FFFFFF] rounded-md shadow">
+                        <div className="text-center space-y-4 pt-12">
+                            <h3 className="text-2xl font-semibold text-[#001222]">Unlimited License</h3>
+                            <h1 className="text-6xl text-[#001222] font-extrabold">$199</h1>
+                            <h4 className="text-xl text-[#001222] font-medium">Single Site license</h4>
+                            <p className="text-base text-black">1 Year Plugin Update1 Year Priority Support All Features Included</p>
+                            <button className="text-lg px-5 py-3 bg-[#0976FF] text-white font-medium rounded shadow pt-3">See All Templates</button>
+                        </div>
+                    </div>
+
+                </div>
+
+                 
+                <div>
+                    <h3 className="text-lg text-[#4898FC] font-medium text-center pt-8">*The discount is applicable for the first-year license only</h3>
+                </div>
             </div>
             
         </div>
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
-
+import logo from "../../../assets/image/logo.png"
+ import footerImg from "../../../assets/image/footer-1.png"
 const Footer = () => {
     return (
-        <div>
+        <div className='w-full h-[600px] bg-[#FFFFFF]'>
+            <div className='w-[78%] h-52 bg-[#0976FF] mx-auto relative bottom-20 rounded-lg shadow-lg '>
+                <div className="flex  items-center px-24">
+                    <div className="flex items-center gap-8">
+                        <div>
+                            <img className="w-40" src={footerImg} alt="" />
+                        </div>
+                        <div>
+                            <h2 className="text-3xl text-white font-semibold">We believe to manage  your data and forms.</h2>
+                            <p className="text-xl font-medium text-white">Stay in touch with us. We'll send you tips and tricks, news and updates, discounts and deals.</p>
+
+                        </div>
+
+                    </div>
+                    <div className="mt-4">
+
+                        <div>
+                            <div className="relative  w-max rounded-lg">
+                                <input className="peer rounded-md border-solid border-2 border-[#FFF] px-14 py-4 text-[#1B8EF8] focus:outline-none bg-[#146FE1]" type="text" placeholder="" />
+                                <label className="absolute -top-2 left-[10px] bg-white px-2 text-xs text-slate-400 duration-300 peer-placeholder-shown:left-[14px] peer-placeholder-shown:top-3  peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-sm peer-focus:-top-2 peer-focus:left-[10px] peer-focus:bg-white peer-focus:text-xs peer-focus:text-blue-400" htmlFor="">Email</label>
+                            </div>
+                            <span className="text-base text-[#A7CEFF] mt-2">We won't send you spam. Unsubscribe at any time.</span>
+                        </div>
+                        <div>
+                            <button className="bg-[#FFFFFF] px-7 py-3 text-xl font-semibold text-[#0976FF] rounded-md shadow mt-2">Subscribe</button>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
             
         </div>
     );
